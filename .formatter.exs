3d3ea2b20f730900081c5dc8fd5e48e09f1f6b1d
Fabien Lamarche-Filion <fabien@marketops.com>
2024-07-05 15:02:17 -0400

    Prevent formatter from modifying API files

--- .formatter.exs
+++ .formatter.exs
@@ -3,5 +3,11 @@
     Enum.flat_map(
       ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
       &Path.wildcard(&1, match_dot: true)
-    ) -- ["lib/ex_uid2/encryption.ex"]
+    ) --
+      [
+        "lib/ex_uid2/encryption.ex",
+        "lib/ex_uid2/api.ex",
+        "lib/ex_uid2/api/request.ex",
+        "lib/ex_uid2/api/response.ex"
+      ]
 ]
