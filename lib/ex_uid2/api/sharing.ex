47ecaceda140e2d0af7c51901ba596fc5ede2437
Fabien Lamarche-Filion <fabien@marketops.com>
2024-07-05 14:57:14 -0400

    Refactor API requests and responses

--- lib/ex_uid2/api/sharing.ex
+++ lib/ex_uid2/api/sharing.ex
@@ -2,7 +2,7 @@ defmodule ExUid2.Api.Sharing do
   @moduledoc """
   This module provides the interface for the DSP to access the UID2 Operator Services's key sharing endpoint.
   """
-  alias ExUid2.Api.Request
+  alias ExUid2.Api
   alias ExUid2.Keyring
 
   @key_sharing_path "/v2/key/sharing"
@@ -11,21 +11,8 @@ defmodule ExUid2.Api.Sharing do
   Request the decryption keys necessary to decrypt UID2 tokens.
   """
   def fetch_keyring() do
-    resp = Request.post_encrypted_request(@key_sharing_path, "")
-    # secret_key = Application.fetch_env!(:ex_uid2, :secret_key) |> :base64.decode()
-
-    IO.inspect(resp: resp)
-
-    # response =
-    #   resp.body
-    #   |> :base64.decode()
-    #   |> Request.decrypt_response(secret_key)
-
-    # raw_body = Map.get(response, "body")
-    raw_body = Map.get(resp, "body")
-
-
-    Keyring.new(raw_body)
+    {:ok, resp} = Api.post_encrypted_request(@key_sharing_path, "")
+    raw_keyring_map = Map.get(resp, "body")
+    Keyring.new(raw_keyring_map)
   end
-
 end
