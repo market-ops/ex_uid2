cae17137ceb51a5b69975f27aba87facc43f4bc7
Fabien Lamarche-Filion <fabien@marketops.com>
2024-07-09 09:52:41 -0400

    Add config

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule ExUid2.MixProject do
       app: :ex_uid2,
       version: "0.1.0",
       elixir: "~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,7 +24,11 @@ defmodule ExUid2.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:req, "~> 0.5.1"}
+      {:req, "~> 0.5.1"},
+      {:plug, "~> 1.0", only: [:test], runtime: false}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
