c0700527a8fdff4094da7be6ea8c2c5fbd6fc284
Fabien Lamarche-Filion <fabien@marketops.com>
2024-07-11 12:53:15 -0400

    Change post function to return tuple

--- lib/ex_uid2/api.ex
+++ lib/ex_uid2/api.ex
@@ -6,21 +6,22 @@ defmodule ExUid2.Api do
     secret_key = get_secret_key()
 
     with {:request_body, {:ok, request_body}} <- {:request_body, Request.make_encrypted_request_body(payload, secret_key, ts)},
-         {:response, %Req.Response{status: 200, body: response_body}} <- {:response, post(request_body, path)},
+         {:response, {:ok, %Req.Response{status: 200, body: response_body}}} <- {:response, post(request_body, path)},
          {:parsed_response, {:ok, parsed_response}} <- {:parsed_response, Response.decrypt_and_parse(response_body, secret_key)} do
       {:ok, parsed_response}
     else
       {:request_body, error} -> error
+      {:response,{:ok, %Req.Response{status: _}} = response} -> {:error, response}
+      {:response, error} -> error
       {:parsed_response, error} -> error
-      {:response, %Req.Response{status: _} = response} -> {:error, response}
     end
   end
 
-  def post(payload, path) do
+  defp post(payload, path) do
     base_url = Application.fetch_env!(:ex_uid2, :base_url)
     api_key = Application.fetch_env!(:ex_uid2, :api_key)
 
-    Req.post!(base_url <> path, auth: {:bearer, api_key}, body: payload)
+    Req.post(base_url <> path, auth: {:bearer, api_key}, body: payload)
   end
 
   def get_secret_key() do
