4a347f5e053d3f81c36b579680053082bde1d0ca
Fabien Lamarche-Filion <fabien@marketops.com>
2024-07-19 16:14:05 -0400

    Add docs and hex options to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,15 +1,22 @@
 defmodule ExUid2.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/market-ops/ex_uid2"
+  @version "0.1.0"
+
   def project do
     [
       app: :ex_uid2,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      docs: docs(),
+      description: description(),
+      package: package(),
+      aliases: aliases(),
+      source_url: @source_url
     ]
   end
 
@@ -24,6 +31,7 @@ defmodule ExUid2.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: [:dev], runtime: false},
       {:req, "~> 0.5"},
       {:plug, "~> 1.16", only: [:test], runtime: false}
     ]
@@ -32,6 +40,32 @@ defmodule ExUid2.MixProject do
   defp aliases do
     [
       test: ["test --no-start"]
+    ]
+  end
+
+  defp description do
+    """
+    UID2 Library for Elixir
+    """
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      groups_for_modules: [
+        Bucketing: [~r/Peep.Buckets/]
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Fabien Lamarche-Filion"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 
