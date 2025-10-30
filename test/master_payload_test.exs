a56e1f1e333eb59af693a3fa8f8323af5f8fa762
Fabien Lamarche-Filion <fabien@marketops.com>
2025-10-30 14:26:14 -0400

    Handle error when the encrypted master payload can't be parsed

---
+++ test/master_payload_test.exs
@@ -0,0 +1,17 @@
+defmodule Test.Encryption.MasterPayload do
+  use ExUnit.Case
+
+  alias ExUid2.Encryption.MasterPayload
+  alias ExUid2.Encryption.EncryptedToken
+
+  test "Invalid V3 Tokens return an error when they can't be parsed" do
+    key = "fake_key"
+
+    master_payload =
+      "invalid" |> Base.encode64()
+
+    token = %EncryptedToken{version: 3, master_payload: master_payload}
+
+    {:error, :cannot_parse_encrypted_payload} = MasterPayload.decrypt(token, key)
+  end
+end
